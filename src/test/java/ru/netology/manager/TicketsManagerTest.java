e53563cca533d3cc678bc4ff0df7f095b1630b42
Yuditskiy-o <Komekadze@gmail.com>
2020-08-23 19:52:29 +0700

    Added new tests for findAllSortTime (100% coverage done)

--- src/test/java/ru/netology/manager/TicketsManagerTest.java
+++ src/test/java/ru/netology/manager/TicketsManagerTest.java
@@ -3,7 +3,10 @@ package ru.netology.manager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Ticket;
+import ru.netology.domain.TicketByTimeAscComparator;
 import ru.netology.repository.TicketsRepository;
+
+import java.util.Comparator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,7 +14,7 @@ class TicketsManagerTest {
     private TicketsRepository repository = new TicketsRepository();
     private TicketsManager manager = new TicketsManager(repository);
     private Ticket first = new Ticket(1, 15000, "OVB", "SVO", 240);
-    private Ticket second = new Ticket(2, 13000, "OVB", "SVO", 240);
+    private Ticket second = new Ticket(2, 13000, "OVB", "SVO", 220);
     private Ticket third = new Ticket(3, 17000, "OVB", "VKO", 240);
 
     @BeforeEach
@@ -47,5 +50,34 @@ class TicketsManagerTest {
         assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void shouldSortTicketsByTravelTime() {
+        manager.ticketAdd(first);
+        manager.ticketAdd(second);
+        Comparator<Ticket> comparator = new TicketByTimeAscComparator();
+        Ticket[] expected = new Ticket[]{second, first};
+        Ticket[] actual = manager.findAllSortTime("OVB", "SVO", comparator);
+        assertArrayEquals(expected, actual);
+    }
 
+    @Test
+    public void shouldBeEmptyIfNoTickets() {
+        manager.ticketAdd(first);
+        manager.ticketAdd(second);
+        manager.ticketAdd(third);
+        Comparator<Ticket> comparator = new TicketByTimeAscComparator();
+        Ticket[] expected = new Ticket[]{};
+        Ticket[] actual = manager.findAllSortTime("DME", "LED", comparator);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnOneIfContains() {
+        manager.ticketAdd(first);
+        manager.ticketAdd(third);
+        Comparator<Ticket> comparator = new TicketByTimeAscComparator();
+        Ticket[] expected = new Ticket[]{first};
+        Ticket[] actual = manager.findAllSortTime("OVB", "SVO", comparator);
+        assertArrayEquals(expected, actual);
+    }
 }
